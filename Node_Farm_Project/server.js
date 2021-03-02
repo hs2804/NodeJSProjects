2ed26a144bf1e3667ee8e42475f4278ca2b71db4
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-03-02 17:13:50 -0500

    Added Code for TourModel and Created a document to test the same

--- Node_Farm_Project/server.js
+++ Node_Farm_Project/server.js
@@ -14,6 +14,40 @@ mongoose.connect(DB, {
 }).then(() => {
   console.log('DB Connection Successfull!');
 });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required:[true, 'Name of the tour is missing']
+  },
+  description : {
+    type: String,
+    required : [true, 'Description of a Tour is missing!']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: String,
+    required : [true, 'Price of a Tour is missing']
+  }
+});
+
+const Tour = mongoose.model('Tour',tourSchema);
+const testTour = new Tour({
+  name: 'The Forest Hikers',
+  price: '$450',
+  description: 'This tour is for forest hikers',
+  rating: 4.9
+});
+testTour.save().then((doc) => {
+  console.log('Successfully Created a document in Collection')
+}).catch((err) => {
+  console.log("Error: ====== " + err);
+})
+
 //console.log(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
