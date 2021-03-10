5b4f56c5e326ecef751b61b4982da7809d98add0
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-03-10 15:53:31 -0500

    Impleted JWT authentication after creating User Module

--- Node_Farm_Project/controllers/usersController.js
+++ Node_Farm_Project/controllers/usersController.js
@@ -1,9 +1,14 @@
-exports.getAllUsers = (req, res) => {
+const User = require("../models/userModel");
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res,next) => {    
+  const users = await User.find();
   res.status(200).json({
     status: 'success',
-    result: 'Request for All Users has been received!',
+    result: users.length,
+    users
   });
-};
+});
 exports.getUser = (req, res) => {
   res.status(200).json({
     status: 'success',
--- Node_Farm_Project/models/userModel.js
+++ Node_Farm_Project/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name : {
@@ -17,14 +18,34 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please enter the password'],
-        minlength: 8
+        minlength: 8,
+        select: false
     },
     passwordConfirm:{
         type: String,
-        required: [true, 'Please confirm the password']
+        required: [true, 'Please confirm the password'],
+        validate:{
+            validator: function(el) {
+                return el === this.password
+            },
+            message: "Confirm Password doesn't match with Password"
+        }
     }
 });
 
+userSchema.pre('save', async function(next){
+
+  if(!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+//Instance Method to compare password
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword){
+  return await bcrypt.compare(candidatePassword, userPassword);
+  
+}
 const User = mongoose.model('User', userSchema);
 
 module.exports= User;
